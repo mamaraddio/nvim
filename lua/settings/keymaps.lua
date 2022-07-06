24bce1bc14078e17dd2cb4e9795147a6e1b9f121
kotvmrc <kotvmrc@gmail.com>
2022-07-06 10:50:37 +0200

    `handlers.lua` changed keymap  leader+Q, `keymaps.lua` added keybindings to quit and save NeoVim

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -6,6 +6,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 
+
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
@@ -28,13 +29,22 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 ------------------------------------
-keymap("n", "<leader>f", ":NvimTreeToggle<CR>", opts)	-- Toggle NvimTree
+keymap("n", "<leader>f", ":Lexplore<CR>", opts)	-- Toggle NvimTree
 keymap("n", "<", "<<", opts)							-- de-indent by pressing "<" once
 keymap("n", ">", ">>", opts)							-- indent by pressing ">" once
 -- Move text up and down
 keymap("n", "<A-j>", ":m .+1<CR>", opts)
 keymap("n", "<A-k>", ":m .-2<CR>", opts)
 ----------------------------------------------
+-- Telescope keybindings
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+-- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>lg", "<cmd>Telescope live_grep<cr>", opts)
+----------------------------------------------
+-- Quit/Save NeoVim by keymaps
+keymap("n", "<leader>q", ":q<cr>", opts)		-- quit with <space>+q
+keymap("n", "<leader>w", ":w<cr>", opts)		-- save with <space>+w
+keymap("n", "<leader>x", ":x<cr>", opts)		-- save and quit with <space>+x
 
 -- Visual --
 -- Stay in indent mode
@@ -49,10 +59,4 @@ keymap("v", "<A-k>", ":m .-2<CR>", opts)
 -- Move text up and down
 keymap("i", "<A-j>", "<Esc>:m .+1<CR>i", opts)
 keymap("i", "<A-k>", "<Esc>:m .-2<CR>i", opts)
-----------------------------------------------
-
---Telescope keybindings
-keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
--- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<leader>lg", "<cmd>Telescope live_grep<cr>", opts)
 ----------------------------------------------
