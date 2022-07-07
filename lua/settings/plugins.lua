f359ccec0bce81646a882898a3e47b24c8162f33
kotvmrc <kotvmrc@gmail.com>
2022-07-07 14:45:14 +0200

    Add `impatient`

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
 	use "wbthomason/packer.nvim" -- Have packer manage itself
 	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+	use "lewis6991/impatient.nvim" -- Speedup NeoVim loading times
 
 	--------------------------------------------------------------------------------------------------------------------------------------------
 	use "kyazdani42/nvim-web-devicons" -- File icons
