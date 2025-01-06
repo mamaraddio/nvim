440cd67689cc049a0116e516e0cf212c458cb17b
mamaraddio <kotvmrc@gmail.com>
2025-01-06 23:00:31 +0000

    Reset main branch

--- lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = "darkplus"
--- local colorscheme = 'hackthebox'
--- local colorscheme = 'tokyonight-night'
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	vim.notify("Colorscheme " .. colorscheme .. " not found!", "warn")
-	return
-end
