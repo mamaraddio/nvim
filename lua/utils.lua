c9de8e87925c445a66d55ed27df4ad59c0ffc432
Marco Amaraddio <kotvmrc@gmail.com>
2025-04-21 21:00:16 +0000

    Complete `Utils.keymap()` function

--- lua/utils.lua
+++ lua/utils.lua
@@ -6,19 +6,24 @@ local Utils = {}
 ---@field rhs string|function The right-hand side of the keymap (the command or function to be executed).
 ---@field opts? vim.keymap.set.Opts Optional additional options for the keymap. Default is an empty table.
 
----@param args Utils.keymap.keymapArgs The arguments for setting the keymap.
+---@param args Utils.keymap.keymapArgs|wk.Spec The arguments for setting the keymap.
 ---@param wk? boolean Optional flag to indicate whether to use which-key for the keymap. Default is false.
 function Utils.keymap(args, wk)
-	local wk = wk or false
-	-- keymap.set(mode: string|string[], lhs: string, rhs: string|function, opts?: vim.keymap.set.Opts)
-	local mode = args.mode or "n"
-	local lhs = args.lhs
-	local rhs = args.rhs
-	local opts = args.opts or {}
-	if not wk then
+	---@private
+	local _wk = wk or false
+	if not _wk then
+		local mode = args.mode or "n"
+		local lhs = args.lhs --[[@as string]]
+		local rhs = args.rhs --[[@as string|function]]
+		local opts = args.opts or {}
 		vim.keymap.set(mode, lhs, rhs, opts)
 	else
-		-- require("which-key").add()
+		local ok, wk = pcall(require, "which-key")
+		if ok then
+			wk.add(args)
+		else
+			vim.notify("which-key not found", vim.log.levels.WARN)
+		end
 	end
 end
 
@@ -30,9 +35,7 @@ function Utils.luacmd(command, args)
 	if not args then
 		return command
 	else
-		return function()
-			command(args)
-		end
+		return function() command(args) end
 	end
 end
 
